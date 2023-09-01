31041f32038a2cdd9ef52e265a1db50c2b3d3ae7
Said Palacio <saidpalacio10@gmail.com>
2023-09-01 10:20:57 -0500

    enunciadov

--- bin/parcial1.dart
+++ bin/parcial1.dart
@@ -1,5 +1,15 @@
 import 'package:parcial1/parcial1.dart' as parcial1;
 
+
+/*Se desea realizar los siguientes criterios de aceptación de 1 requerimiento para el módulo de productos:
+
+a.Se debe tener un objeto de ventas donde se anexará cada producto que sea seleccionado
+
+b.Cada producto que estén 15 días a vencer se le otorgará un 15% de descuento
+
+c.Mostrar el total que se debe pagar*/
+
 void main(List<String> arguments) {
   print('Hello world: ${parcial1.calculate()}!');
 }
+
